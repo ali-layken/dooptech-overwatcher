178a08fb7da7191efcc74b7481611b4ae411e359
Ahmed Usman <usmanaa@umich.edu>
2025-06-26 19:12:15 -0400

    init - crop filter

---
+++ src/plugin-main.cpp
@@ -0,0 +1,23 @@
+/*
+HeroWatcher
+*/
+
+#include <obs-module.h>
+#include <plugin-support.h>
+
+extern struct obs_source_info hero_tracker;
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+bool obs_module_load(void)
+{
+	obs_register_source(&hero_tracker);
+	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	return true;
+}
+
+void obs_module_unload(void)
+{
+	obs_log(LOG_INFO, "plugin unloaded");
+}
