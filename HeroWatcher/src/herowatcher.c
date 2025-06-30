ae173ce9a2da34108fa5b07bcc34a2629a4dbdb6
Ahmed Usman <usmanaa@umich.edu>
2025-06-29 20:58:58 -0400

    Revert "true pass tru"
    This reverts commit 766f381e6d634c8af48db3fa7c6407316cb667c8.

--- HeroWatcher/src/herowatcher.c
+++ HeroWatcher/src/herowatcher.c
@@ -4,7 +4,6 @@
 struct hero_watcher_data {
     // OBS Plugin API Members
     obs_source_t *context;
-	gs_effect_t *effect;
     int width;
 	int height;
 
@@ -31,36 +30,20 @@ static const char *hero_watcher_get_name(void *unused)
 static void *hero_watcher_create(obs_data_t *settings, obs_source_t *context)
 {
 	struct hero_watcher_data *filter = bzalloc(sizeof(*filter));
-	char *effect_path = obs_module_file("crop_filter.effect");
-	blog(LOG_INFO, "Trying to load effect from: %s", effect_path);
 	filter->context = context;
 
-	obs_enter_graphics();
-	filter->effect = gs_effect_create_from_file(effect_path, NULL);
-	obs_leave_graphics();
-	bfree(effect_path);
-
-	if (!filter->effect) {
-		bfree(filter);
-		return NULL;
-	}
+	obs_source_update(context, settings);
 
 	obs_source_t *target = obs_filter_get_target(filter->context);
 	filter->width = obs_source_get_base_width(target);
 	filter->height = obs_source_get_base_height(target);
 
-	obs_source_update(context, settings);
 	return filter;
 }
 
 static void hero_watcher_destroy(void *data)
 {
 	struct hero_watcher_data *filter = data;
-
-	obs_enter_graphics();
-	gs_effect_destroy(filter->effect);
-	obs_leave_graphics();
-
 	bfree(filter);
 }
 
@@ -80,15 +63,7 @@ static void hero_watcher_render(void *data, gs_effect_t *effect)
 {
 	UNUSED_PARAMETER(effect);
 	struct hero_watcher_data *filter = data;
-	obs_source_t *target = obs_filter_get_target(filter->context);
-
-	if (!target || !filter->effect){
-		return;
-	}
-
-	if (obs_source_process_filter_begin(filter->context, GS_RGBA, OBS_NO_DIRECT_RENDERING)) {
-		obs_source_process_filter_end(filter->context, filter->effect, filter->width, filter->height);
-	}
+	obs_source_skip_video_filter(filter->context);
 }
 
 struct obs_source_info hero_watcher = {
